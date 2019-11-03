65b215aa634f02635625d78f95d62d85e0acf207
quiks <quiksilver4210@gmail.com>
2019-11-04 02:07:07 +1000

    add lesson 1_capture

---
+++ 1_capture/main.cpp
@@ -0,0 +1,45 @@
+#include "opencv2/opencv.hpp"
+#include <string>
+
+const std::string file_directory = "../../examples/";
+
+int showVideo(const std::string &win_name) {
+  cv::VideoCapture capture(file_directory + "dog.mp4");
+  cv::Mat frame;
+  if (!capture.isOpened()) {
+    std::cerr << "Can't open video file" << std::endl;
+    return 1;
+  }
+  while (capture >> frame, !frame.empty()) {
+    cv::imshow(win_name, frame);
+    if (cv::waitKey(20) >= 0) {
+      std::cout << "You stopped playing video" << std::endl;
+      break;
+    }
+  }
+  std::cout << "Video is ended" << std::endl;
+  return 0;
+}
+void createWindow(const std::string &win_name) {
+  cv::namedWindow(win_name, cv::WINDOW_NORMAL);
+}
+
+int showImage(const std::string &win_name) {
+  cv::Mat img = cv::imread(file_directory + "apples.jpeg");
+  if (img.empty()) {
+    std::cerr << "Can't open image" << std::endl;
+    return 1;
+  }
+  cv::imshow(win_name, img);
+  cv::waitKey(0);
+  return 0;
+}
+
+int main() {
+  std::string win_name = "new window";
+  createWindow(win_name);
+  showImage(win_name);
+  showVideo(win_name);
+  cv::destroyAllWindows();
+  return 0;
+}
